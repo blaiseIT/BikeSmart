68a117ce6c9aa7eaa1e0bb467ad426c3cc6df989
Blaise Petric <blaiseIT@users.noreply.github.com>
2018-02-10 17:17:08 -0500

    Added muscle% and balance calculation, firebase
    Cadence is not yet implemented, nor is async firebase write

--- data-collect.py
+++ data-collect.py
@@ -1,10 +1,22 @@
-import Adafruit_ADS1x15, datetime, time
+import Adafruit_ADS1x15, datetime, time, pyrebase
 
-from firebase import firebase
-#firebase = firebase.FirebaseApplication('https://muscle-bike.firebaseio.com/', None)
+config = {
+    "apiKey": "",
+    "authDomain": "",
+    "databaseURL": "",
+    "projectId": "",
+    "storageBucket": "",
+    "messagingSenderId": ""
+}
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+db = firebase.database()
 
 adc = Adafruit_ADS1x15.ADS1115()
 GAIN = 1
+left = 0
+right = 20000
+counter = 0
 
 print('Reading ADS1x15 values, press Ctrl-C to quit...')
 # Print nice channel column headers.
@@ -17,11 +29,22 @@ while True:
     values = [0]*4
     for i in range(4):
         values[i] = adc.read_adc(i, gain=GAIN)
-    #res = firebase.put('piData-1',str(z),{'Distance':'nil','Muscle':str(values[0])})
-    #res2 = firebase.put('live','muscle-left',values[0])
     current = datetime.datetime.now()
     elapsed = current - start
     print int(elapsed.total_seconds() * 1000)
     print('      {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*values))
     # Pause for half a second.
-    time.sleep(0.06)
+
+    left += values[0]#left
+    right += values[1] #rich        
+    if counter % 10 == 9:
+        print (100*left/(left+right))    #this is left power percent
+        print (left+right)/4000      #This is muscle %
+        #print last_cadence
+        data = {"cadence": counter, "balance": (100*left/(left+right)), "muscle": (left+right)/4000}
+    db.child("livetest").set(data)
+        left=0
+        right=0
+    else:
+        time.sleep(0.06)
+    counter += 1
